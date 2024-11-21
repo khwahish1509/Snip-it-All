001110d4d2ce263902d275f7a4b2cc8516a14fed
data-dude42 <ss8820788@gmail.com>
2024-11-21 11:58:22 -0500

    working on the debouncing.js

---
+++ debouncing.js
@@ -0,0 +1,7 @@
+function debounce (func, delay){
+    let timer;
+    return function (...args) {
+        clearTimeout(timer);
+        timer = setTimeout(() => func.apply(this, args), delay);
+    };
+}
